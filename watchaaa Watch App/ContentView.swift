3aaf7315d590f72993642b498b57bcfc18286953
Iago Ramos <mirandolaiago@gmail.com>
2022-11-04 16:52:11 -0400

    first commit

---
+++ watchaaa Watch App/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Logger Watch WatchKit Extension
+//
+//  Created by MacBook Pro on 2020/05/03.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var logStarting = false
+  
+    @ObservedObject var sensorLogger = SensorLogManager()
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.logStarting.toggle()
+                
+                if self.logStarting {
+                    self.sensorLogger.startUpdate(60.0)
+                }
+                else {
+                    self.sensorLogger.stopUpdate()
+                    
+                }
+            }) {
+                if self.logStarting {
+                    Image(systemName: "pause.circle")
+                }
+                else {
+                    Image(systemName: "play.circle")
+                }
+            }
+            
+            VStack {
+                VStack {
+                Text("Device Motion").font(.headline)
+                HStack {
+                    Text(String(format: "%.2f", self.sensorLogger.accX))
+                    Spacer()
+                    Text(String(format: "%.2f", self.sensorLogger.accY))
+                    Spacer()
+                    Text(String(format: "%.2f", self.sensorLogger.accZ))
+                }.padding(.horizontal)
+                }
+                
+//                VStack {
+//                Text("Gyroscope").font(.headline)
+//                HStack {
+//                    Text(String(format: "%.2f", self.sensorLogger.gyrX))
+//                    Spacer()
+//                    Text(String(format: "%.2f", self.sensorLogger.gyrX))
+//                    Spacer()
+//                    Text(String(format: "%.2f", self.sensorLogger.gyrX))
+//                }.padding(.horizontal)
+//                }
+
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchaaa Watch App/watchaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchaaaApp.swift
+//  watchaaa Watch App
+//
+//  Created by Iago Ramos on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct watchaaa_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
